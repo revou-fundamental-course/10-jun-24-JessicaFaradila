f2a26122e62d0fd115302c036e41f64224bad964
JessicaFaradila <jessicafaradila023@gmail.com>
2024-06-20 19:47:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 function convertToCelsius() {
     var valNum = parseFloat(document.getElementById("inputFahrenheit").value);
     if (!isNaN(valNum)) {
-      document.getElementById("inputCelsius").value = ((valNum - 32) / 1.8).toFixed(2);
+      var celsius = ((valNum - 32) / 1.8).toFixed(2);
+      document.getElementById("inputCelsius").value = celsius;
+      document.getElementById("calculationMethod").value = `${valNum}°F - 32) / 1.8 = ${celsius}°C`;
     }
   }
   
   function convertToFahrenheit() {
     var valNum = parseFloat(document.getElementById("inputCelsius").value);
     if (!isNaN(valNum)) {
-      document.getElementById("inputFahrenheit").value = ((valNum * 1.8) + 32).toFixed(2);
+      var fahrenheit = ((valNum * 1.8) + 32).toFixed(2);
+      document.getElementById("inputFahrenheit").value = fahrenheit;
+      document.getElementById("calculationMethod").value = `${valNum}°C * (9/5) + 32 = ${fahrenheit}°F`;
     }
   }
   
   function resetFields() {
     document.getElementById("inputFahrenheit").value = '';
     document.getElementById("inputCelsius").value = '';
+    document.getElementById("calculationMethod").value = '';
   }
